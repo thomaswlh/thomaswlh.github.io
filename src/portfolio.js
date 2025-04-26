afda9e7bcf7c84c601d72231f2500a51908cbfab
thomaswu-wlh-06 <thomaswu.wlh@gmail.com>
2025-04-27 00:57:46 +0800

    Update portfolio.js
    change personal info and disable some sections due to lack of info

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,71 +1,75 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://thomaswu-wlh-06.github.io/',
+  title: 'TW.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Thomas Wu',
+  role: 'passionate CS Student studying in University of Macau',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    '',
+  resume: '',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/thomaswu06/',
+    github: 'https://github.com//thomaswu-wlh-06',
   },
 }
 
-const projects = [
-  // projects can be added an removed
-  // if there are no projects, Projects section won't show up
-  {
-    name: 'Project 1',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 2',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-]
+// const projects = [
+//   // projects can be added an removed
+//   // if there are no projects, Projects section won't show up
+//   {
+//     name: 'Project 1',
+//     description:
+//       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+//     stack: ['SASS', 'TypeScript', 'React'],
+//     sourceCode: 'https://github.com',
+//     livePreview: 'https://github.com',
+//   },
+//   {
+//     name: 'Project 2',
+//     description:
+//       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+//     stack: ['SASS', 'TypeScript', 'React'],
+//     sourceCode: 'https://github.com',
+//     livePreview: 'https://github.com',
+//   },
+//   {
+//     name: 'Project 3',
+//     description:
+//       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+//     stack: ['SASS', 'TypeScript', 'React'],
+//     sourceCode: 'https://github.com',
+//     livePreview: 'https://github.com',
+//   },
+// ]
 
-const skills = [
-  // skills can be added or removed
-  // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
-  'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
-]
+const projects = []
+
+// const skills = [
+//   // skills can be added or removed
+//   // if there are no skills, Skills section won't show up
+//   'HTML',
+//   'CSS',
+//   'JavaScript',
+//   'TypeScript',
+//   'React',
+//   'Redux',
+//   'SASS',
+//   'Material UI',
+//   'Git',
+//   'CI/CD',
+//   'Jest',
+//   'Enzyme',
+// ]
+
+const skills = []
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'thomaswu.wlh@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
