91da72f0624ccdff67f87843db7d65266862424e
thomaswu-wlh-06 <thomaswu.wlh@gmail.com>
2025-04-27 15:44:36 +0800

    Update skills and clean up portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -17,34 +17,6 @@ const about = {
   },
 }
 
-// const projects = [
-//   // projects can be added an removed
-//   // if there are no projects, Projects section won't show up
-//   {
-//     name: 'Project 1',
-//     description:
-//       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-//     stack: ['SASS', 'TypeScript', 'React'],
-//     sourceCode: 'https://github.com',
-//     livePreview: 'https://github.com',
-//   },
-//   {
-//     name: 'Project 2',
-//     description:
-//       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-//     stack: ['SASS', 'TypeScript', 'React'],
-//     sourceCode: 'https://github.com',
-//     livePreview: 'https://github.com',
-//   },
-//   {
-//     name: 'Project 3',
-//     description:
-//       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-//     stack: ['SASS', 'TypeScript', 'React'],
-//     sourceCode: 'https://github.com',
-//     livePreview: 'https://github.com',
-//   },
-// ]
 
 const projects = [
   {
@@ -56,30 +28,13 @@ const projects = [
   }
 ]
 
-// const skills = [
-//   // skills can be added or removed
-//   // if there are no skills, Skills section won't show up
-//   'HTML',
-//   'CSS',
-//   'JavaScript',
-//   'TypeScript',
-//   'React',
-//   'Redux',
-//   'SASS',
-//   'Material UI',
-//   'Git',
-//   'CI/CD',
-//   'Jest',
-//   'Enzyme',
-// ]
 
 const skills = [
+  'Python',
   'HTML',
   'CSS',
   'JavaScript',
-  'React',
   'Git',
-  'Material UI'
 ]
 
 const contact = {
