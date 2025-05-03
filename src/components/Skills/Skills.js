9c2eb3b0a18f95f5804dc15ec4370dffb2431b84
thomaswu-wlh-06 <thomaswu.wlh@gmail.com>
2025-05-03 16:26:31 +0800

    Add skill percentage circle bar display

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,8 +1,15 @@
+import { CircularProgressbar } from 'react-circular-progressbar';
+// import 'react-circular-progressbar/dist/styles.css';
 import uniqid from 'uniqid'
+import { useContext } from 'react';
+import { ThemeContext } from '../../contexts/theme'
 import { skills } from '../../portfolio'
 import './Skills.css'
 
+
 const Skills = () => {
+  const [{ themeName }] = useContext(ThemeContext)
+  
   if (!skills.length) return null
 
   return (
@@ -11,7 +18,25 @@ const Skills = () => {
       <ul className='skills__list'>
         {skills.map((skill) => (
           <li key={uniqid()} className='skills__list-item btn btn--plain'>
-            {skill}
+            {skill.name}
+            <CircularProgressbar
+              value={skill.percentage}
+              text={`${skill.percentage}%`}
+              strokeWidth="15"
+              styles={{
+                path: {
+                  stroke: themeName === 'dark' ? '#90a0d9' : '#2978b5'
+                },
+                trail: {
+                  stroke: themeName === 'dark' ? '#2a2f4c' : '#d6d6d6'
+                },
+                text: {
+                  fill: themeName === 'dark' ? '#90a0d9' : '#2978b5',
+                  fontSize: '30px',
+                  transform: 'translateY(3px)'
+                }
+              }}
+            />
           </li>
         ))}
       </ul>
